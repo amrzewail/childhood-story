4b95f1416f22d21942d17b207abf6453180f6c0f
yousef <youssefadmire@gmail.com>
2022-02-17 23:03:31 +0200

    change the movement of characters consider with camera

--- Assets/Scripts/Characters/Components/SystemInput.cs
+++ Assets/Scripts/Characters/Components/SystemInput.cs
@@ -15,7 +15,15 @@ public class SystemInput : MonoBehaviour, IInput
     public void onMove(InputAction.CallbackContext context)
     {
         axis = context.ReadValue<Vector2>();
-        if(axis.magnitude != 0)
+
+        float radians = -Camera.main.transform.eulerAngles.y * Mathf.PI / 180;
+        float cos = Mathf.Cos(radians), sin = Mathf.Sin(radians);
+        var ax = axis;
+        ax.x = cos * axis.x - sin * axis.y;
+        ax.y = cos * axis.y + sin * axis.x;
+
+        axis = ax;
+        if (axis.magnitude != 0)
         {
             absAxis = axis;
         }
