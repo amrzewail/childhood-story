2e28972eb76f0313a4ff4d348650e75b61545f0c
kingbero44 <kingbero44@gmail.com>
2022-02-19 01:34:27 +0200

    mirror start

---
+++ Assets/Scripts/Interactables/MirrorInteractable.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorInteractable : MonoBehaviour,IInteractable
+{
+    public bool CanInteract()
+    {
+        
+    }
+
+    public void Interact(IDictionary<string, object> data)
+    {
+        
+    }
+
+    public bool IsComplete()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
