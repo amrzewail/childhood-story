9b82c12747758d18b1eb112f193ad47999ddbd97
kingbero44 <kingbero44@gmail.com>
2022-02-19 07:57:39 +0200

    done teleporting mirrors

--- Assets/Scripts/Interactables/MirrorInteractable.cs
+++ Assets/Scripts/Interactables/MirrorInteractable.cs
@@ -1,33 +1,40 @@
+using Characters;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MirrorInteractable : MonoBehaviour,IInteractable
 {
-    public bool CanInteract()
-    {
-        
-    }
+    [SerializeField] private Transform pivot = null;
 
+    // Update is called once per frame
     public void Interact(IDictionary<string, object> data)
     {
-        
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        if (pivot == null) { return; }
+        IActor actor = (IActor)data["actor"];
+        actor.transform.position = pivot.position;
+       
+        actor.transform.rotation = Quaternion.Euler(eulerRotation.x, 90+eulerRotation.y, eulerRotation.z);
+        Debug.Log("Mirror accessed by" + ((IActor)data["actor"]).transform.name);
+
+
+
     }
+    public bool CanInteract()
+    {
+        //code
+        return true;
+    }
+
+   
 
     public bool IsComplete()
     {
-        
+        //code
+        return true ;
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
