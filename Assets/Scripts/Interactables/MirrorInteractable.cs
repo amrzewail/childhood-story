bb047cd50413aa674437049f300e5aa304d19a3e
kingbero44 <kingbero55@gmail.com>
2022-02-20 08:05:44 +0200

    update mirror interactable

--- Assets/Scripts/Interactables/MirrorInteractable.cs
+++ Assets/Scripts/Interactables/MirrorInteractable.cs
@@ -7,8 +7,12 @@ using UnityEngine;
 public class MirrorInteractable : MonoBehaviour,IInteractable
 {
     [SerializeField]private Transform pivot;
+    [SerializeField]private Transform particle;
     [SerializeField]private MirrorInteractable mirror;
     private bool caninteract = true, iscomplete = false;
+    [SerializeField]private float speed;
+    private Vector3 originalPos;
+
     // Update is called once per frame
     public void Interact(IDictionary<string, object> data)
     {
@@ -34,13 +38,26 @@ public class MirrorInteractable : MonoBehaviour,IInteractable
 
 
     }
-
+    private void Awake()
+    {
+        //originalPos = particle.position;
+    }
     private IEnumerator TimeDelay(IDictionary<string, object> data)
     {
-        
+
         IActor actor = (IActor)data["actor"];
         actor.transform.gameObject.GetComponentsInChildren<Renderer>().ToList().ForEach(x => x.enabled = false);
-        yield return new WaitForSeconds(2);
+        //Vector3 a = particle.transform.position;
+        //Vector3 b = mirror.transform.position;
+        particle.gameObject.SetActive(true);
+        //particle.transform.position = Vector3.Lerp(a, b, t);
+        Vector3 test = new Vector3(2.5f, 0, 0.038f);
+        Debug.Log("particle first mirror position "+ test);
+        particle.transform.position = Vector3.MoveTowards(test, mirror.transform.position, 2f * Time.deltaTime);
+        yield return new WaitForSeconds(3);
+        particle.transform.position = originalPos;
+        Debug.Log("last position is the first one" + particle.transform.position);
+        particle.gameObject.SetActive(false);
         actor.transform.position = mirror.pivot.position;
         actor.transform.gameObject.GetComponentsInChildren<Renderer>().ToList().ForEach(x => x.enabled = true);
 
