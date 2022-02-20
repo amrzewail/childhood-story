97e71f889a9bbeb5268f5b4fc53d79b697a46208
kingbero44 <kingbero55@gmail.com>
2022-02-21 00:27:49 +0200

    mirror interactable new update

--- Assets/Scripts/Interactables/MirrorInteractable.cs
+++ Assets/Scripts/Interactables/MirrorInteractable.cs
@@ -9,60 +9,52 @@ public class MirrorInteractable : MonoBehaviour,IInteractable
     [SerializeField]private Transform pivot;
     [SerializeField]private Transform particle;
     [SerializeField]private MirrorInteractable mirror;
-    private bool caninteract = true, iscomplete = false;
-    [SerializeField]private float speed;
+    private bool caninteract, iscomplete;
+    [SerializeField]private float timedelay;
+    private float distance;
     private Vector3 originalPos;
 
-    // Update is called once per frame
+
+    private void Awake()
+    {
+        originalPos = particle.position;
+    }
+    private void Update()
+    {
+        if(CanInteract() == false && IsComplete() == false)
+        {
+            particle.position = Vector3.MoveTowards(particle.position, mirror.transform.position, (distance/timedelay) * Time.deltaTime);
+        }
+    }
     public void Interact(IDictionary<string, object> data)
     {
-        
-
+        particle.position = originalPos;
+        iscomplete = false;
+        caninteract = false;
         Vector3 eulerRotation = transform.rotation.eulerAngles;
         if (pivot == null || mirror == null) { return; }
 
         IActor actor = (IActor)data["actor"];
         StartCoroutine(TimeDelay(data));
-       
-        //actor.transform.position = mirror.pivot.position;
-       
         actor.transform.rotation = Quaternion.Euler(eulerRotation.x, 90+eulerRotation.y, eulerRotation.z);
-        //actor.gameObject.SetActive(true);
         Debug.Log("Mirror accessed by" + ((IActor)data["actor"]).transform.name);
-        caninteract = false;
-        iscomplete = true;
-        CanInteract();
-        IsComplete();
-        //Debug.Log("can interact : " + CanInteract());
-        //Debug.Log("is complete : " + IsComplete());
-
-
     }
-    private void Awake()
-    {
-        //originalPos = particle.position;
-    }
+
     private IEnumerator TimeDelay(IDictionary<string, object> data)
     {
-
         IActor actor = (IActor)data["actor"];
         actor.transform.gameObject.GetComponentsInChildren<Renderer>().ToList().ForEach(x => x.enabled = false);
-        //Vector3 a = particle.transform.position;
-        //Vector3 b = mirror.transform.position;
         particle.gameObject.SetActive(true);
-        //particle.transform.position = Vector3.Lerp(a, b, t);
-        Vector3 test = new Vector3(2.5f, 0, 0.038f);
-        Debug.Log("particle first mirror position "+ test);
-        particle.transform.position = Vector3.MoveTowards(test, mirror.transform.position, 2f * Time.deltaTime);
-        yield return new WaitForSeconds(3);
+  
+        distance = Vector3.Distance(originalPos, mirror.transform.position);
+
+        yield return new WaitForSeconds(timedelay);
+        iscomplete = true;
+        caninteract = true;
         particle.transform.position = originalPos;
-        Debug.Log("last position is the first one" + particle.transform.position);
         particle.gameObject.SetActive(false);
         actor.transform.position = mirror.pivot.position;
         actor.transform.gameObject.GetComponentsInChildren<Renderer>().ToList().ForEach(x => x.enabled = true);
-
-
-        //transform.GetComponent<Renderer>().material.color = Color.red;
     }
     private void OnDrawGizmos()
     {
