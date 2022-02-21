55e8de86f93835d177ca918e77355ad6194134e4
amrzewail <amrzewail@hotmail.com>
2022-02-21 02:01:31 +0200

    some pivot fixes and sizes

--- Assets/Scripts/Component/TriggerButton.cs
+++ Assets/Scripts/Component/TriggerButton.cs
@@ -2,6 +2,7 @@ using Characters;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TriggerButton : MonoBehaviour
 {
@@ -10,14 +11,23 @@ public class TriggerButton : MonoBehaviour
     [SerializeField]
     float pressDistance=0.2f;
 
+    public UnityEvent OnButtonDown;
+    public UnityEvent OnButtonUp;
+
     public void ButtonDown(Collider col)
     {
-        if( col.GetComponent<IActor>() != null)
+        if (col.GetComponent<IActor>() != null)
+        {
             renderer.transform.Translate(Vector3.down * pressDistance);
+            OnButtonDown?.Invoke();
+        }
     }
     public void ButtonUp(Collider col)
     {
         if (col.GetComponent<IActor>() != null)
+        {
             renderer.transform.Translate(Vector3.up * pressDistance);
+            OnButtonUp?.Invoke();
+        }
     }
 }
