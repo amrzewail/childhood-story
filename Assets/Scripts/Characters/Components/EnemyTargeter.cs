69a7ea195c1a17365bea4693d7486061000def38
Abdelrahman Adam <abdoss190@gmail.com>
2022-04-01 15:47:32 +0200

    MakeTargeter

--- Assets/Scripts/Characters/Components/EnemyTargeter.cs
+++ Assets/Scripts/Characters/Components/EnemyTargeter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyTrigger : MonoBehaviour
+public class EnemyTargeter : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
